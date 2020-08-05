794f24cdc965681cbe91c622bdca5486a2422c91
Filip Jerga <jerga99@gmail.com>
2020-08-05 17:33:19 +0200

    Counter Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   // The set od components, directives, pipes (declarables) that belong to
   // this module
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent
   ],
   // The set of NgModules whose exported declarables are available to template
   // in this module
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,13 @@
+
+
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss']
+})
+export class CounterComponent {
+  counter = 0;
+}
