6ee7b5f8f18705f239429686ae6bfe059bae418f
Filip Jerga <jerga99@gmail.com>
2020-08-05 17:16:31 +0200

    Decorators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,23 @@
 import { Component } from '@angular/core';
 
 
+function TestDecorator(configuration) {
+  return function(target) {
+    target.testData = configuration;
+    target.meta = {
+      prop1: 'a',
+      prop2: 'b'
+    }
+  }
+}
 
 // Decorator
 // Syntactic sugar for a function thats wraps data structure and provides
 // aditional functionalities to it
+@TestDecorator({
+  name: 'App Component Test',
+  testingProp: 'Just some informations'
+})
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,8 +27,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   title = 'Hello I am new value'
+  name = AppComponent['testData'].name
 
   generateNumber() {
     return 1;
   }
 }
+
