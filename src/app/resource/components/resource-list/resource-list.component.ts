368eec209cacbc0497222340291d8bb0668bf3d7
Filip Jerga <jerga99@gmail.com>
2020-08-10 09:20:59 +0200

    Resources data

--- src/app/resource/components/resource-list/resource-list.component.ts
+++ src/app/resource/components/resource-list/resource-list.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-resource-list',
   templateUrl: './resource-list.component.html',
   styleUrls: ['./resource-list.component.scss']
 })
-export class ResourceListComponent implements OnInit {
+export class ResourceListComponent  {
+  @Input() resources: [{_id: string, title: string, description: string}];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get jsonResources() {
+    this.resources[0].title;
+    return JSON.stringify(this.resources);
   }
-
 }
--- src/app/resource/resource.component.ts
+++ src/app/resource/resource.component.ts
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
 })
 export class ResourceComponent {
 
+  public resources = [{
+    _id: '1',
+    title: 'Resource 1',
+    description: 'Resource desc 1',
+    link: 'https://google.com',
+    type: 'video'
+  },{
+    _id: '2',
+    title: 'Resource 2',
+    description: 'Resource desc 2',
+    link: 'https://google.com',
+    type: 'blog'
+  },{
+    _id: '3',
+    title: 'Resource 3',
+    description: 'Resource desc 3',
+    link: 'https://google.com',
+    type: 'book'
+  }]
 }
 
