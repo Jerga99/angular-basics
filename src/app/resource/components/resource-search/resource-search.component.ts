07842aaed3291bfc2aa0b69140f64ab5c0705ecc
Filip Jerga <jerga99@gmail.com>
2020-08-13 10:31:12 +0200

    Search from event rxjs

--- src/app/resource/components/resource-search/resource-search.component.ts
+++ src/app/resource/components/resource-search/resource-search.component.ts
@@ -1,11 +1,29 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+  AfterViewInit } from '@angular/core';
+
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-resource-search',
   templateUrl: './resource-search.component.html',
   styleUrls: ['./resource-search.component.scss']
 })
-export class ResourceSearchComponent {
+export class ResourceSearchComponent implements AfterViewInit {
+
+  @ViewChild('searchInput') input: ElementRef;
+
   @Output() onSearch = new EventEmitter<string>();
+
+  ngAfterViewInit() {
+    fromEvent(this.input.nativeElement, 'keyup')
+      .subscribe((e: any) => {
+        this.onSearch.emit(e.target.value)
+      })
+  }
 
 }
